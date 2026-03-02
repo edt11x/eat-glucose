84d9d1c6b852862d5c27cf67f8296d17371e522c
edt11x <github@nimbusiii.com>
2026-03-01 21:25:44 -0500

    Initial Commit

---
+++ edt-glucose/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  edt-glucose
+//
+//  Created by Edward Thompson on 3/1/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
